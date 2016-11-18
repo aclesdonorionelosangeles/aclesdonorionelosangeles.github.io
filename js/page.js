fbecfc917ec59f45265fad7aacdbc003b993bcd3
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-11-18 14:49:24 -0300

    add 60& of Acles Don Orione Page

---
+++ js/page.js
@@ -0,0 +1,24 @@
+function EasyPeasyParallax() {
+	scrollPos = $(this).scrollTop();
+	$('.p1').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+  $('.p2').css({
+		'background-position' : '50% ' + (-scrollPos/8)+"px"
+	});
+  $('.p3').css({
+		'background-position' : '70% ' + (-scrollPos/52)+"px"
+	});
+  $('.p4').css({
+	  	'background-position' : '90% ' + (-scrollPos/52)+"px"
+  });
+	$('.parallax-text').css({
+		'margin-top': (scrollPos/2)+"px",
+		'opacity': 1-(scrollPos/230)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		EasyPeasyParallax();
+	});
+});
